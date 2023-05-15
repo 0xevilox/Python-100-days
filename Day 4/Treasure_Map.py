82595e82f378126c509de303690c0380c4969285
Vignesh <83863784+0xevilox@users.noreply.github.com>
2023-05-15 22:30:17 +0530

    Update Treasure_Map.py

--- Day 4/Treasure_Map.py
+++ Day 4/Treasure_Map.py
@@ -3,19 +3,21 @@ row1 = ["😊","️😊","️😊","😊"]
 row2 = ["😊","😊","️😊","😊"]
 row3 = ["😊️","😊","😊","😊"]
 row4 = ["😊️","😊","😊","😊"]
-map = [row1, row2, row3,row4]
-print(f"{row1}\n{row2}\n{row3}")
+map = [row1, row2, row3,row4]   # Nested List
+print(f"{row1}\n{row2}\n{row3}\n{row4}")
 position = input("Where do you want to put the treasure? ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
-var = int((position[0])) - 1
-var_1 = int((position[1])) - 1
 
-selected = map[var_1]
-selected[var] = "X"
+coloum = int(position[0]) - 1
+row = int(position[1]) - 1
+
+select = map[row]
+select[coloum] = "X"
+
 
 #Write your code above this row 👆
 
 # 🚨 Don't change the code below 👇
-print(f"{row1}\n{row2}\n{row3}")
+print(f"{row1}\n{row2}\n{row3}\n{row4}")
